4642beb22b0e79f1c269bbf32cf8e971fbabc190
Lennert Verhaegen <lennert.verhaegen@student.ucll.be>
2025-12-18 10:14:44 +0100

    Achtergrond

--- Rainbow_joyride-Copy/Achtergrond.py
+++ Rainbow_joyride-Copy/Achtergrond.py
@@ -3,7 +3,7 @@ import pygame
 from Startup import *
 from player_colors import *
 
-BACKGROUND_PATH = "projectweek-team_7\\Rainbow_joyride-Copy\\Background\\background\content.png"
+BACKGROUND_PATH = "Rainbow_joyride-Copy\\Background\\background\\background_image.png"
 BG_SPEED = 2
 background, bg_loaded = load_image(BACKGROUND_PATH, (WIDTH, HEIGHT))
 
